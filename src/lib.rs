3c5e07ac660f414412045ade79cdf05cbd1b8817
kana-rus <kanarus786@gmail.com>
2024-11-28 12:26:39 +0900

    add #[cfg(debug_assertions)] pub fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,33 @@
+//! This crate's `lib.rs` is:
+//! 
+//! ```
+//! pub fn add(left: u64, right: u64) -> u64 {
+//!     left + right
+//! }
+//! 
+//! #[cfg(debug_assertions)]
+//! pub fn debug_add(left: u64, right: u64) {
+//!     dbg!(add(left, right));
+//! }
+//! 
+//! #[cfg(test)]
+//! pub fn test_add(left: u64, right: u64, expected: u64) {
+//!     assert_eq!(add(left, right), expected);
+//! }
+//! ```
+//! 
+//! Check what items are available by user.
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+#[cfg(debug_assertions)]
+pub fn debug_add(left: u64, right: u64) {
+    dbg!(add(left, right));
+}
+
 #[cfg(test)]
-pub fn test(left: u64, right: u64, expected: u64) {
+pub fn test_add(left: u64, right: u64, expected: u64) {
     assert_eq!(add(left, right), expected);
 }
